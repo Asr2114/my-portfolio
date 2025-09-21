0817471f424909274cf366bcc5fa7fd4d8ab4aea
Asr2114 <105535943+Asr2114@users.noreply.github.com>
2025-09-21 16:13:21 +0530

    Updated Resume Link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1kK_J30Lxdpffknt1kMPOvjFDoTww8h0C/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1DJVrZ-DPSfJaCnah0JFyAhYGVwimgGcK/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
